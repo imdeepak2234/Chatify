70d84cf6a5efb3f669417098521d06fd31d9f104
imdeepak2234 <imdeepak2234@gmail.com>
2024-06-17 14:24:21 +0530

    ChatInput.jsx modified to make it more responsive

--- public/src/components/ChatInput.jsx
+++ public/src/components/ChatInput.jsx
@@ -7,19 +7,19 @@ import Picker from "emoji-picker-react";
 export default function ChatInput({ handleSendMsg }) {
   const [msg, setMsg] = useState("");
   const [showEmojiPicker, setShowEmojiPicker] = useState(false);
+
   const handleEmojiPickerhideShow = () => {
     setShowEmojiPicker(!showEmojiPicker);
   };
 
   const handleEmojiClick = (event, emojiObject) => {
-    let message = msg;
-    message += emojiObject.emoji;
-    setMsg(message);
+    const newMessage = msg + emojiObject.emoji;
+    setMsg(newMessage);
   };
 
   const sendChat = (event) => {
     event.preventDefault();
-    if (msg.length > 0) {
+    if (msg.trim().length > 0) {
       handleSendMsg(msg);
       setMsg("");
     }
@@ -36,9 +36,9 @@ export default function ChatInput({ handleSendMsg }) {
       <form className="input-container" onSubmit={(event) => sendChat(event)}>
         <input
           type="text"
-          placeholder="type your message here"
-          onChange={(e) => setMsg(e.target.value)}
+          placeholder="Type your message here"
           value={msg}
+          onChange={(e) => setMsg(e.target.value)}
         />
         <button type="submit">
           <IoMdSend />
@@ -50,93 +50,102 @@ export default function ChatInput({ handleSendMsg }) {
 
 const Container = styled.div`
   display: grid;
-  align-items: center;
-  grid-template-columns: 5% 95%;
+  grid-template-columns: auto 1fr;
   background-color: #080420;
-  padding: 0 2rem;
-  @media screen and (min-width: 720px) and (max-width: 1080px) {
-    padding: 0 1rem;
-    gap: 1rem;
+  padding: 1rem;
+
+  @media screen and (min-width: 720px) {
+    grid-template-columns: auto 1fr auto; /* Adjust column layout for wider screens */
   }
+
   .button-container {
     display: flex;
     align-items: center;
     color: white;
     gap: 1rem;
+
     .emoji {
       position: relative;
+
       svg {
         font-size: 1.5rem;
         color: #ffff00c8;
         cursor: pointer;
       }
+
       .emoji-picker-react {
         position: absolute;
         top: -350px;
         background-color: #080420;
         box-shadow: 0 5px 10px #9a86f3;
         border-color: #9a86f3;
+
         .emoji-scroll-wrapper::-webkit-scrollbar {
           background-color: #080420;
           width: 5px;
+
           &-thumb {
             background-color: #9a86f3;
           }
         }
+
         .emoji-categories {
           button {
             filter: contrast(0);
           }
         }
+
         .emoji-search {
           background-color: transparent;
           border-color: #9a86f3;
         }
+
         .emoji-group:before {
           background-color: #080420;
         }
       }
     }
   }
+
   .input-container {
-    width: 100%;
-    border-radius: 2rem;
     display: flex;
     align-items: center;
-    gap: 2rem;
     background-color: #ffffff34;
+    border-radius: 2rem;
+    padding: 0.5rem;
+    margin-left: 1rem;
+
     input {
-      width: 90%;
-      height: 60%;
+      flex: 1;
+      height: 100%;
       background-color: transparent;
       color: white;
       border: none;
       padding-left: 1rem;
       font-size: 1.2rem;
 
-      &::selection {
-        background-color: #9a86f3;
+      &::placeholder {
+        color: #d1d1d1;
       }
+
       &:focus {
         outline: none;
       }
     }
+
     button {
-      padding: 0.3rem 2rem;
-      border-radius: 2rem;
+      padding: 0.5rem;
+      margin-left: 0.5rem;
+      border-radius: 50%;
+      background-color: #9a86f3;
+      border: none;
       display: flex;
       justify-content: center;
       align-items: center;
-      background-color: #9a86f3;
-      border: none;
-      @media screen and (min-width: 720px) and (max-width: 1080px) {
-        padding: 0.3rem 1rem;
-        svg {
-          font-size: 1rem;
-        }
-      }
+      cursor: pointer;
+
       svg {
-        font-size: 2rem;
+        font-size: 1.5rem;
         color: white;
       }
     }
