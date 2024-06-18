18c0b8dc8a4782e1b7140d0b50ca0254a43624da
imdeepak2234 <imdeepak2234@gmail.com>
2024-06-18 15:47:53 +0530

    Changes made for deployment

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,13 @@
-export const host = "http://localhost:5000";
+// export const host = "http://localhost:5000";
+// export const loginRoute = `${host}/api/auth/login`;
+// export const registerRoute = `${host}/api/auth/register`;
+// export const logoutRoute = `${host}/api/auth/logout`;
+// export const allUsersRoute = `${host}/api/auth/allusers`;
+// export const sendMessageRoute = `${host}/api/messages/addmsg`;
+// export const recieveMessageRoute = `${host}/api/messages/getmsg`;
+// export const setAvatarRoute = `${host}/api/auth/setavatar`;
+
+export const host = "https://your-backend-vercel-url.vercel.app";
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
